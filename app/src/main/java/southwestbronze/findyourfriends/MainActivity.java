4e712693d98e1102c699d17b8a4e237902646fa5
Spencer Walton <spencerwalton@dhcp-206-12-52-178.eduroam.wireless.ubc.ca>
2016-02-28 12:15:59 -0800

    Cleaned up UI a bit - removed extra menu functions we won't use

--- app/src/main/java/southwestbronze/findyourfriends/MainActivity.java
+++ app/src/main/java/southwestbronze/findyourfriends/MainActivity.java
@@ -99,14 +99,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_gallery) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.hide(fragment);
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         if(fragment != null){
